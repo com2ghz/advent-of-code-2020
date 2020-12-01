f86c937283f5744258a6c6cc97bf98606b0287d6
Gökhan Orhun <gokhan@orhun.nl>
2020-12-01 21:48:42 +0100

    Solve day 1part 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,4 +1,22 @@
 package day1
 
-class Day1 {
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.streams.toList
+
+fun main() {
+    val numbers = Files.readAllLines(Paths.get("src/main/resources/day1.txt")).stream()
+        .mapToInt(Integer::parseInt)
+        .toList()
+
+    var result = 0;
+    for (outer in numbers) {
+        for (inner in numbers) {
+            if (outer + inner == 2020) {
+                result = outer * inner
+                break
+            }
+        }
+    }
+    println(result);
 }
