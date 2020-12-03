22d74233504ebf390c3ad9aaaa9ff4d89f56d717
Gökhan Orhun <gokhan@orhun.nl>
2020-12-03 20:44:19 +0100

    Rename

--- src/main/kotlin/day3/Part2.kt
+++ src/main/kotlin/day3/Part2.kt
@@ -7,17 +7,17 @@ fun main() {
     val lines = Files.readAllLines(Paths.get("src/main/resources/day3.txt"))
 
     val count = arrayOf(
-        slopeDown(lines, 1, 1),
-        slopeDown(lines, 3, 1),
-        slopeDown(lines, 5, 1),
-        slopeDown(lines, 7, 1),
-        slopeDown(lines, 1, 2),
+        slope(lines, 1, 1),
+        slope(lines, 3, 1),
+        slope(lines, 5, 1),
+        slope(lines, 7, 1),
+        slope(lines, 1, 2),
     ).map { println(it); it }.reduce { acc, i -> acc * i }
 
     println(count)
 }
 
-private fun slopeDown(lines: MutableList<String>, right: Int, down: Int, ): Int {
+private fun slope(lines: MutableList<String>, right: Int, down: Int, ): Int {
     var stepRight = 0;
     return lines
         .filterIndexed { index, s -> index % down == 0 }
