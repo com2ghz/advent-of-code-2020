7ef0a74c4cb0d443b194067283be4768421022b4
Gökhan Orhun <gokhan@orhun.nl>
2020-12-03 20:43:01 +0100

    Also in java

---
+++ src/main/kotlin/day3/Part2.java
@@ -0,0 +1,34 @@
+package day3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Part2 {
+    public static void main(String[] args) throws IOException {
+        List<String> strings = Files.readAllLines(Paths.get("src/main/resources/day3.txt"));
+
+        Integer count = Stream.of(
+                slope(strings, 1, 1),
+                slope(strings, 3, 1),
+                slope(strings, 5, 1),
+                slope(strings, 7, 1),
+                slope(strings, 1, 2)
+        ).reduce((a, b) -> a * b)
+                .orElse(0);
+        System.out.println(count);
+    }
+
+    private static int slope(List<String> strings, int right, int down) {
+        int trees = 0;
+        for (int i = 0, stepsRight = 0; i < strings.size(); i = i + down, stepsRight += right) {
+            int x = stepsRight % strings.get(i).length();
+            if (strings.get(i).charAt(x) == '#') {
+                trees++;
+            }
+        }
+        return trees;
+    }
+}
